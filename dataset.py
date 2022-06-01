824bb001dd7744b506827fa96466a15d53787507
samuel <samuel@sotelo.io>
2022-06-01 18:03:23 +0200

    New changes

--- dataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-
-files = ["./dataset_benign.csv", "./dataset_malware.csv"]
-
-data = pd.concat(map(pd.read_csv, files), ignore_index=True)
-
-data = data.sample(frac=1, ignore_index=True)
-
-data.to_csv('./dataset.csv')
